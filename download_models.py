77a22835bd6905ed9bf3d2bd4477f070fe70bb76
Tobias Fischer <tobias.fischer@qut.edu.au>
2021-08-27 08:11:13 +1000

    Do not check SSL certificates

--- download_models.py
+++ download_models.py
@@ -2,6 +2,7 @@
 
 import os
 import urllib.request
+import ssl
 from patchnetvlad.tools import PATCHNETVLAD_ROOT_DIR
 
 
@@ -25,26 +26,27 @@ def ask_yesno(question):
 
 
 def download_all_models(ask_for_permission=False):
+    context = ssl._create_unverified_context()
     dest_dir = os.path.join(PATCHNETVLAD_ROOT_DIR, 'pretrained_models')
     if not ask_for_permission or ask_yesno("Auto-download pretrained models into " + dest_dir + " (takes around 2GB of space)? Yes/no."):
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA128.pth.tar")):
             print('Downloading mapillary_WPCA128.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/vvr0jizjti0z2LR/download", os.path.join(dest_dir, "mapillary_WPCA128.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/vvr0jizjti0z2LR/download", os.path.join(dest_dir, "mapillary_WPCA128.pth.tar"), context=context)
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA512.pth.tar")):
             print('Downloading mapillary_WPCA512.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/DFxbGgFwh1y1wAz/download", os.path.join(dest_dir, "mapillary_WPCA512.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/DFxbGgFwh1y1wAz/download", os.path.join(dest_dir, "mapillary_WPCA512.pth.tar"), context=context)
         if not os.path.isfile(os.path.join(dest_dir, "mapillary_WPCA4096.pth.tar")):
             print('Downloading mapillary_WPCA4096.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/ZgW7DMEpeS47ELI/download", os.path.join(dest_dir, "mapillary_WPCA4096.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/ZgW7DMEpeS47ELI/download", os.path.join(dest_dir, "mapillary_WPCA4096.pth.tar"), context=context)
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA128.pth.tar")):
             print('Downloading pittsburgh_WPCA128.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/2ORvaCckitjz4Sd/download", os.path.join(dest_dir, "pittsburgh_WPCA128.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/2ORvaCckitjz4Sd/download", os.path.join(dest_dir, "pittsburgh_WPCA128.pth.tar"), context=context)
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA512.pth.tar")):
             print('Downloading pittsburgh_WPCA512.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/WKl45MoboSyB4SH/download", os.path.join(dest_dir, "pittsburgh_WPCA512.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/WKl45MoboSyB4SH/download", os.path.join(dest_dir, "pittsburgh_WPCA512.pth.tar"), context=context)
         if not os.path.isfile(os.path.join(dest_dir, "pittsburgh_WPCA4096.pth.tar")):
             print('Downloading pittsburgh_WPCA4096.pth.tar')
-            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/1aoTGbFjsekeKlB/download", os.path.join(dest_dir, "pittsburgh_WPCA4096.pth.tar"))
+            urllib.request.urlretrieve("https://cloudstor.aarnet.edu.au/plus/s/1aoTGbFjsekeKlB/download", os.path.join(dest_dir, "pittsburgh_WPCA4096.pth.tar"), context=context)
         print('Downloaded all pretrained models.')
 
 if __name__ == "__main__":
